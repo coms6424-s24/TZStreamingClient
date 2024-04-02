f7ddf277adda59b6825395c09992b99aa2149b48
Yizhi Wang <yw4174@columbia.edu>
2024-04-02 21:28:14 +0000

    server setup

---
+++ RA/client.py
@@ -0,0 +1,6 @@
+import httpx # pip install httpx
+URL = "http://localhost:8000"
+
+
+if __name__ == "__main__":
+    
---
+++ Server/server.sh
@@ -0,0 +1,3 @@
+# ffmpeg -re -f video4linux2 -i /dev/video0 -vcodec libx264 -vprofile baseline -acodec aac -strict -2 -f flv rtmp://localhost/show/stream
+VIDEO_SOURCE=big_buck_bunny_240p_30mb.mp4
+ffmpeg -re -i $VIDEO_SOURCE -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://localhost/stream
