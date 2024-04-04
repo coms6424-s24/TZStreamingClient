49c01d7841b7c7ce238cea6e6d29162aa08bba46
Yizhi Wang <yw4174@columbia.edu>
2024-04-04 04:42:26 +0000

    1212

--- HLS_Server/server.sh
+++ HLS_Server/server.sh
@@ -1,4 +1,5 @@
 # ffmpeg -re -f video4linux2 -i /dev/video0 -vcodec libx264 -vprofile baseline -acodec aac -strict -2 -f flv rtmp://localhost/show/stream
 # VIDEO_SOURCE=big_buck_bunny_240p_30mb.mp4
 # ffmpeg -re -i $VIDEO_SOURCE -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -f flv rtmp://localhost/stream
+
 python3 hls-stream/server.py hls-stream/testdata/m1-all_Custom.m3u8
